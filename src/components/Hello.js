3700af3ed7eb251fb420be171e0133e06477b8fd
Andrew Minai <andrewminai24@gmail.com>
2019-02-23 22:21:26 -0800

    intitial

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Hello extends Component {
+    //static propTypes = {}
+
+    state = {}
+
+    render () {
+        
+        return (<div>Hello</div>);
+        
+    }
+}
